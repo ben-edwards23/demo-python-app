9dd9e8abe273cdb11e247bcbadfd1c745e9e3eb5
Ben Edwards <ben-edwards@hotmail.co.uk>
2025-09-16 13:13:00 +0100

    always_on has to be false

--- infra/main.tf
+++ infra/main.tf
@@ -62,7 +62,6 @@ resource "azurerm_linux_web_app" "web" {
   service_plan_id     = azurerm_service_plan.plan.id
   https_only          = true
   tags                = local.tags
-  site_config { always_on = false }
 
   identity {
     type = "SystemAssigned"
@@ -73,6 +72,7 @@ resource "azurerm_linux_web_app" "web" {
     application_stack {
       python_version = "3.12"
     }
+    always_on = false
   }
 
   app_settings = {
