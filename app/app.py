5ed8e14f209f54031987fa362791b78bdba9a777
Ben Edwards <ben-edwards@hotmail.co.uk>
2025-09-16 16:18:33 +0100

    Let's go

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", message="Hello! This app is running in a container on Azure Web Apps!")
+    return render_template("index.html", message="Hello there! This app is running in a container on Azure Web Apps!")
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
